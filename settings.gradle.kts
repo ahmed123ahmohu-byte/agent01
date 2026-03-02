d2c4a36a802df571b85e405909f168d7bb6ee3d6
ahmed123ahmohu-byte <ahmed123ahmohu@gmail.com>
2026-03-02 22:16:13 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,17 @@
+pluginManagement {
+    repositories {
+        google()           // هذا ما كان ينقصك لتحميل Plugin الأندرويد
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "DevSplitAI"
 include(":app")
